0360d31b575cc79c35f0a285a5022f55ffa3bc58
Max Kremmel <xing@synapse.plus.com>
2005-12-06 10:12:48 +0000

    merge recent changes into HEAD

---
+++ jscripts/plugins/autosave/langs/cs.js
@@ -0,0 +1,11 @@
+/**
+ * Czech lang variables 
+ * encoding: utf-8
+ *  
+ * $Id: cs.js,v 1.2 2005/12/06 10:12:46 squareing Exp $ 
+ */  
+
+tinyMCE.addToLang('',{
+autosave_unload_msg : 'Změny, které jste udělal(a) budou ztraceny, jestliže opustíte tuto stránku.'
+});
+
---
+++ jscripts/plugins/autosave/langs/sk.js
@@ -0,0 +1,13 @@
+/**
+ * Slovak lang variables 
+ * encoding: utf-8
+ * 
+ * @author Vladimir VASIL vvasil@post.sk
+ *    
+ * $Id: sk.js,v 1.2 2005/12/06 10:12:46 squareing Exp $ 
+ */  
+
+tinyMCE.addToLang('',{
+autosave_unload_msg : 'Zmeny, ktoré ste urobyl(a) budú stratené, ak opustíte túto stránku.'
+});
+
