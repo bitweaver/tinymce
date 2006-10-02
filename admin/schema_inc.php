c32b235008df45a24f30ad777662354a6dd3d6e7
Max Kremmel <xing@synapse.plus.com>
2006-10-02 13:56:39 +0000

    include version number

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -4,6 +4,7 @@ $gBitInstaller->registerPackageInfo( TINYMCE_PKG_NAME, array(
 	'description' => "Tiny MCE is a 'What You See Is What You Get' textarea HTML editor. It works with MSIE and Gecko based browsers such as Mozilla and Firefox.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
 	'important' => 'When using this WYSIWYG, we recommend that you either use HTML as the only content format or at least set it as default (formats can be set in Administration --> Liberty --> Liberty plugins).',
+	'version' => '2.0.6.1',
 ) );
 
 $gBitInstaller->registerPreferences( TINYMCE_PKG_NAME, array(
